b40ec09caf71eac6a990043569d72318151add29
LinasAdsSupport <106584220+LinasAdsSupport@users.noreply.github.com>
2022-11-22 11:12:39 +0200

    Async test

---
+++ Assets/PreExport.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+public class PreExport : MonoBehaviour
+{
+    public void Start()
+    {
+        PreExportMethod();
+    }
+    public async void PreExportMethod()
+    {
+        Debug.Log("Start Asynch method: "+DateTime.Now.ToString());
+        await AwatiForPause();
+        Debug.Log("End Asynch method: "+DateTime.Now.ToString());
+    }
+    async Task<bool> AwatiForPause()
+    {
+        Debug.Log("Doing the async method");
+        await Task.Delay(5000);
+        return true;
+    }
+}
